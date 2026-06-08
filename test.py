3526c7d6cb859647ca9c7f8f83f50131330ba4cb
Themi Megas <Tcm4760@gmail.com>
2026-06-07 20:31:08 -0500

    Made json output pretty print

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ with open("./test_data/track2.bin", "rb") as f:
     gpmf = GPMF(f)
 
 with open("./out.json", "w") as f:
-    json.dump(gpmf.to_dict(), f)
+    json.dump(gpmf.to_dict(), f, indent=4)
