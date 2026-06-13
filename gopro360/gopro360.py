8fd381ab9ffd3090b125b70052a2e4b34fe92960
Themi Megas <Tcm4760@gmail.com>
2026-06-13 17:09:31 -0500

    read_frames automatically selects the correct tracks based on metadata (no longer hardcoded). This is in anticipation for support of all video modes, including timelapse

--- gopro360/gopro360.py
+++ gopro360/gopro360.py
@@ -3,7 +3,6 @@ import ffmpeg
 import numpy as np
 from .read_meta import get_meta, GPSSample
 from .gpmf import GPMF
-from typing import Tuple
 
 
 class GoPro360File:
@@ -11,6 +10,7 @@ class GoPro360File:
         self.file_path = file_path
 
     def get_meta(self) -> List[GPSSample]:
+        """Gets GPMF metadata (contains sensor information)"""
         probe = ffmpeg.probe(self.file_path)
         gpmf_stream_info = next((stream for stream in probe["streams"] if stream["codec_tag_string"] == "gpmd"), None)
         if gpmf_stream_info is None:
@@ -28,27 +28,37 @@ class GoPro360File:
         process.wait()
         return get_meta(gpmf)
 
+    def get_video_streams(self):
+        """Returns metadata for the two video streams"""
+        probe = ffmpeg.probe(self.file_path)
+        streams_meta = (stream for stream in probe["streams"] if stream.get("codec_name") == "hevc")
+        stream1 = next(streams_meta)
+        stream2 = next(streams_meta)
+        return stream1, stream2
+
     def read_frames(self):
-        process = (
+        """Reads raw frames out from file"""
+        stream_meta = self.get_video_streams()
+        process1 = (
             ffmpeg
             .input(self.file_path)
-            .output("pipe:", format='rawvideo', pix_fmt="rgb24")
+            .output("pipe:", format='rawvideo', pix_fmt="rgb24", **{'map': f"0:{stream_meta[0]['index']}"})
             .run_async(pipe_stdout=True)
         )
         process2 = (
             ffmpeg
             .input(self.file_path)
-            .output("pipe:", format='rawvideo', pix_fmt="rgb24", **{'map': "0:4"})
+            .output("pipe:", format='rawvideo', pix_fmt="rgb24", **{'map': f"0:{stream_meta[1]['index']}"})
             .run_async(pipe_stdout=True)
         )
         while True:
-            in_bytes = process.stdout.read(1344 * 4096 * 3)
-            in_bytes2 = process.stdout.read(1344 * 4096 * 3)
-            if not in_bytes:
+            in_bytes1 = process1.stdout.read(1344 * 4096 * 3)
+            in_bytes2 = process1.stdout.read(1344 * 4096 * 3)
+            if not in_bytes1:
                 break
-            frame = (
+            frame1 = (
                 np
-                .frombuffer(in_bytes, np.uint8)
+                .frombuffer(in_bytes1, np.uint8)
                 .reshape([1344, 4096, 3])
             )
             frame2 = (
@@ -56,6 +66,6 @@ class GoPro360File:
                 .frombuffer(in_bytes2, np.uint8)
                 .reshape([1344, 4096, 3])
             )
-            yield frame, frame2
-        process.wait()
+            yield frame1, frame2
+        process1.wait()
         process2.wait()
